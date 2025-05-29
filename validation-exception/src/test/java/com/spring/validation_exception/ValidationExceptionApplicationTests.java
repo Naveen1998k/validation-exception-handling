5ac43c6b33966bb8562b741a45780c306e2501a6
K Naveen <naveenkokkiligadda2001@gmail.com>
2025-05-29 10:54:28 +0530

    Commit pages

---
+++ validation-exception/src/test/java/com/spring/validation_exception/ValidationExceptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.validation_exception;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidationExceptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
